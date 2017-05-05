58ea241111cc4ff1b8d1df6a64228887d8f80eb0
westtexascentral <westtexascentral@gmail.com>
2017-05-04 20:45:52 -0500

    Added an adminlayout and admin.index.html.twig file.

--- blog/src/AppBundle/Controller/AdminDashboardController.php
+++ blog/src/AppBundle/Controller/AdminDashboardController.php
@@ -5,16 +5,17 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+* @Route("/admin")
+*/
 class AdminDashboardController extends Controller
 {
     /**
-     * @Route("/admin")
+     * @Route("/")
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:AdminDashboard:index.html.twig', array(
-            // ...
-        ));
+        return $this->render('admin/index.html.twig');
     }
 
 }
